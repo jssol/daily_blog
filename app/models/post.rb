a173b49dea0e305ea3d86251e8b4321a8c087c56
Jonathan Sivahera <jsivahera@icloud.com>
2022-08-12 22:15:56 +0200

    Create custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ApplicationRecord
   has_many :comments
+  has_many :likes
   belongs_to :author, class_name: 'User'
+
+  after_save :update_posts_counter
+
+  def latest_comments
+    Comment.order(created_at: :desc).includes([:author]).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
