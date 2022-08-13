a2f01f719f58c9977eb5228fad0c30b60ff56e97
Jonathan Sivahera <jsivahera@icloud.com>
2022-08-13 19:06:44 +0200

    Revert "Add validations for the Post model"
    This reverts commit 2f371e767324c16eb7805601301dc73a8ae421ab.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,6 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
 
-  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-
   def latest_comments
     Comment.order(created_at: :desc).includes([:author]).limit(5)
   end
