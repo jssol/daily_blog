e39837230a080d7c80bf691c42e5de2884152324
Jonathan Sivahera <jsivahera@icloud.com>
2022-08-20 16:54:36 +0200

    Add #create action for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,4 +14,33 @@ class PostsController < ApplicationController
       format.html { render :new, locals: { post: @post } }
     end
   end
+
+  def create
+    # new object from params
+    @post = Post.new(post_params)
+
+    # respond_to block
+    respond_to do |format|
+      format.html do
+        if @post.save
+          # success message
+          flash[:success] = 'Post created successfully'
+          # redirect to index
+          redirect_to user_posts_url
+        else
+          flash.now[:error] = 'Error: Post could not be created'
+          # render new
+          render :new, locals: { post: @post }
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    post_params = params.require(:post).permit(:user_id, :title, :text)
+    post_params[:author_id] = current_user.id
+    post_params
+  end
 end
