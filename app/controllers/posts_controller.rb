0c21eb76ba521a45dc4c1b39eefe5d76ff14a0a6
Jonathan Sivahera <jsivahera@icloud.com>
2022-08-20 16:53:52 +0200

    Add #new action for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.where(author_id: params[:user_id])
+    @posts = Post.where(author_id: params[:user_id]).order(created_at: :desc)
   end
 
   def show
     @post = Post.where(author_id: params[:user_id]).find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+    @user = current_user
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
   end
 end
