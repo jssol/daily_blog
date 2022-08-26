a861949343c41293367dfdfb054b033813034660
Jonathan Sivahera <jsivahera@icloud.com>
2022-08-26 21:13:58 +0200

    Fix linters

--- spec/features/post_index_page_spec.rb
+++ spec/features/post_index_page_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Post', type: :feature do
   before :each do
-    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    @second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Teacher from Mexico.')
+    @second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                               bio: 'Teacher from Poland.')
     @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
-    Comment.create(post: @first_post, author: @second_user, text: 'Hi Tom!' )
-    Comment.create(post: @first_post, author: @second_user, text: 'Hi Mom!' )
+    Comment.create(post: @first_post, author: @second_user, text: 'Hi Tom!')
+    Comment.create(post: @first_post, author: @second_user, text: 'Hi Mom!')
     visit user_posts_path(@first_user.id)
   end
 
