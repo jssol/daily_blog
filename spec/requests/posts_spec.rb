9b19740423b9d3ef9aacbad130c30a9c72052fc4
Jonathan Sivahera <jsivahera@icloud.com>
2022-08-26 22:05:34 +0200

    Update failing assertions

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'tests the #index action' do
-    before(:each) do
-      get '/users/12/posts'
+    before :each do
+      @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                bio: 'Teacher from Mexico.')
+      @second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                bio: 'Teacher from Poland.')
+      @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
+      Comment.create(post: @first_post, author: @second_user, text: 'Hi Tom!')
+      Comment.create(post: @first_post, author: @second_user, text: 'Hi Mom!')
+      get user_posts_path(@first_user.id)
     end
 
     it 'should render the correct template' do
@@ -11,7 +18,7 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it 'should have the correct placeholder text' do
-      expect(response.body).to include('Here is a list of posts for a given user')
+      expect(response.body).to include('Tom')
     end
 
     it 'should have a correct response status' do
@@ -20,8 +27,15 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'tests the #show action' do
-    before(:each) do
-      get '/users/12/posts/11'
+    before :each do
+      @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                bio: 'Teacher from Mexico.')
+      @second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                bio: 'Teacher from Poland.')
+      @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
+      Comment.create(post: @first_post, author: @second_user, text: 'Hi Tom!')
+      Comment.create(post: @first_post, author: @second_user, text: 'Hi Mom!')
+      get user_post_path(@first_user.id, @first_post.id)
     end
 
     it 'should render the correct template' do
@@ -29,7 +43,7 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it 'should have the correct placeholder text' do
-      expect(response.body).to include('Here is a single post made by a given user')
+      expect(response.body).to include('Lilly: Hi Tom!')
     end
 
     it 'should have a correct response status' do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'should have the correct placeholder text' do
-      expect(response.body).to include('Here is a list of all the users')
+      expect(response.body).to include('Name: Gomez')
     end
 
     it 'should have a correct response status' do
@@ -20,8 +20,14 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'tests the #show action' do
-    before(:each) do
-      get '/users/22'
+    before :each do
+      first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                bio: 'Teacher from Poland.')
+      first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+      Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
+      Comment.create(post: first_post, author: second_user, text: 'Hi Mom!')
+      get user_path(first_user.id)
     end
 
     it 'should render the correct template' do
@@ -29,7 +35,7 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'should have the correct placeholder text' do
-      expect(response.body).to include('Here is a single given user page')
+      expect(response.body).to include('Mexico')
     end
 
     it 'should have a correct response status' do
