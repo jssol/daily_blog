ed03db8b8b099e5a8f10c15905a2ce1e627d6e94
Jonathan Sivahera <jsivahera@icloud.com>
2022-08-26 22:13:35 +0200

    Fix linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -21,7 +21,8 @@ RSpec.describe 'Users', type: :request do
 
   describe 'tests the #show action' do
     before :each do
-      first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                               bio: 'Teacher from Mexico.')
       second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                                 bio: 'Teacher from Poland.')
       first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
